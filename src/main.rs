fd69e58d323de9a5854b05d28de3aa9a2e36480f
poly000 <1348292515@qq.com>
2025-07-11 17:11:37 +0800

    fix: support non pipe stdout

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use rustix::pipe::{IoSliceRaw, SpliceFlags, fcntl_setpipe_size, vmsplice};
-use std::{io::stdout, os::fd::AsFd as _};
+use std::{io::stdout, mem::transmute, os::fd::AsFd as _};
 
 const BATCH: usize = 8;
+const TIMES: usize = 1024;
+const CONTENT: [u8; 4] = [b'y', b'e', b's', b'\n'];
 
 fn main() {
-    fcntl_setpipe_size(stdout(), 1048576).expect("failed to set pipe size");
+    _ = fcntl_setpipe_size(stdout(), 1048576);
+
+    let buf: [u8; TIMES * CONTENT.len()] = unsafe { transmute([CONTENT; TIMES]) };
+    let bufs: Vec<_> = (0..BATCH).map(|_| IoSliceRaw::from_slice(&buf)).collect();
 
     let stdout = stdout().lock();
     let fd = stdout.as_fd();
-    let buf = [b'y'; 4096];
-    let bufs: Vec<_> = (0..BATCH).map(|_| IoSliceRaw::from_slice(&buf)).collect();
     loop {
         unsafe {
             _ = vmsplice(fd, &bufs, SpliceFlags::empty());
