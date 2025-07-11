64ab2343f6ac3fef30db35dc9afe1a3c8cc165c0
poly000 <1348292515@qq.com>
2025-07-11 12:32:11 +0800

    perf: vmsplice, 15.6GiB/s now

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use std::{io::stdout, os::fd::AsFd as _};
+#![feature(maybe_uninit_slice)]
 
-use rustix::{io::write, pipe::fcntl_setpipe_size};
+use rustix::pipe::{IoSliceRaw, SpliceFlags, fcntl_setpipe_size, vmsplice};
+use std::{io::stdout, mem::MaybeUninit, os::fd::AsFd as _};
+
+const BATCH: usize = 8;
 
 fn main() {
     fcntl_setpipe_size(stdout(), 1048576).expect("failed to set pipe size");
 
     let stdout = stdout().lock();
     let fd = stdout.as_fd();
+    let buf = [b'y'; 4096];
+    let mut bufs = [const { MaybeUninit::uninit() }; BATCH];
+    for i in 0..BATCH {
+        bufs[i].write(IoSliceRaw::from_slice(&buf));
+    }
+    let bufs = unsafe { bufs.assume_init_ref() };
     loop {
-        _ = write(fd, &[b'y'; 4096]);
+        unsafe {
+            _ = vmsplice(fd, &bufs, SpliceFlags::empty());
+        };
     }
 }
